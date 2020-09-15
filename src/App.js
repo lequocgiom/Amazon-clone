02df471320bf1f01ac4a4b719927f37899056814
lequocgiom <letrung95@gmail.com>
2020-09-15 21:47:13 +0700

    Setup router, added header component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Header";
 function App() {
+  // BEM
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <h1 className="">Orders</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <h1>Payment</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>Home</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
