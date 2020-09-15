a3c4ec7a8fb3357b5f319a24f325745fc6284b52
lequocgiom <letrung95@gmail.com>
2020-09-15 21:58:30 +0700

    added the banner.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
 function App() {
   // BEM
   return (
@@ -25,7 +26,7 @@ function App() {
           </Route>
           <Route path="/">
             <Header />
-            <h1>Home</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
